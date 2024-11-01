f9bf5ace56ec0f0d628a5b4bf5feb547b9a58417
hadyannabil <hadyannabilsk@gmail.com>
2024-11-01 20:57:53 +0700

    Update and rename FilkomTourAndTravel.java to TravelSewa.java

--- TravelSewa.java
+++ TravelSewa.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class FilkomTourAndTravel {
+public class TravelSewa {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
