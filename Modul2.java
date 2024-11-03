c7095d6425d6e84116034078f6bbd668545d8815
hadyannabil <hadyannabilsk@gmail.com>
2024-11-03 18:52:34 +0700

    Update Modul2.java

--- Modul2.java
+++ Modul2.java
@@ -84,7 +84,13 @@ public class Modul2 {
                     horsePower = tmphorsePower;
                     panjangArray++;
                 }
-                case 2 -> lihatData(noPlat, merkMobil, warnaMobil, tahunkeluaran, jenisMobil, horsePower, panjangArray);
+                case 2 -> {
+                    if (panjangArray == 0) {
+                    System.out.println("Belum ada transaksi untuk ditampilkan.");
+                    } else {
+                        lihatData(noPlat, merkMobil, warnaMobil, tahunkeluaran, jenisMobil, horsePower, panjangArray);
+                    } 
+                }
                 case 0 -> {
                     run = false;
                     System.out.println("Anda telah menyelesaikan penginputan data mobil.");
@@ -197,7 +203,6 @@ public class Modul2 {
                 default -> System.out.println("Pilihan tidak valid.");
             }
         }
-        in.close();
     }
 
     public static void lihatData(String[] noPlat, String[] merkMobil, String[] warnaMobil, short[] tahunkeluaran, String[] jenisMobil, int[] horsePower, int panjangArray) {
@@ -205,12 +210,13 @@ public class Modul2 {
         System.out.println("=================================================================================================");
         System.out.printf("%-5s %-15s %-15s %-15s %-15s %-15s %-15s\n", "No", "Plat No", "Merk", "Warna", "Tahun", "Jenis", "Horse Power");
         System.out.println("-------------------------------------------------------------------------------------------------");
+        
         for (int i = 0; i < panjangArray; i++) {
             System.out.printf("%-5d %-15s %-15s %-15s %-15d %-15s", (i + 1), noPlat[i], merkMobil[i], warnaMobil[i], tahunkeluaran[i], jenisMobil[i]);
             if (jenisMobil[i].equals("Supercar")) {
-                System.out.printf("%-15d\n", horsePower[i]);
+                System.out.printf(" %-15d\n", horsePower[i]);
             } else {
-                System.out.printf("%-15c\n", '-');
+                System.out.printf(" %c\n", '-');
             }
         }
         System.out.println("=================================================================================================");
