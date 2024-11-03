ea699d03cb33aaa3710cc34c464d1f3c929defd9
Ihya' Ulumuddin <pebehakun45@gmail.com>
2024-11-03 20:32:13 +0700

    Improve ui + validasi cek gaji

--- Modul1.java
+++ Modul1.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 
 public class Modul1 {
     public static void main(String[] args) {
+        
         Scanner in = new Scanner(System.in);
         String[][] dataString = new String[5][0];
         long[][] dataLong = new long[2][0];
@@ -63,6 +64,9 @@ public class Modul1 {
                         System.out.printf("%-20s: ", "No. Rekening");       dataLongTmp[1][panjangDataTmp-1] = in.nextLong();
                         System.out.printf("%-20s: ", "Jenis Kelamin");      dataCharTmp[panjangDataTmp-1] = in.next().charAt(0);
                         in.nextLine();
+
+                        System.out.println("Karyawan Berhasil Ditambahkan");
+
                         panjangDataTmp++;
                         inputLagiBenar = false;
 
@@ -101,119 +105,160 @@ public class Modul1 {
                 case 2 -> tampilNama(dataString, dataLong, dataChar);
                 case 3 -> {
 
-                    System.out.printf("%-20s: ", "Nama Karyawan"); String name = in.nextLine();
-                    System.out.printf("%-20s: ", "Kategori"); String kategori = in.nextLine();
-                    int indeksKaryawan = cariKaryawan(name, dataString, kategori, daftarKategori);
-                    
-                    if (indeksKaryawan == -1) {
-
-                        System.out.println("Karyawan tidak ditemukan.");
-
-                    } else {
-
-                        System.out.printf("%-20s: ", "Jam Lembur"); int jamLembur = in.nextInt();
-
-                        double gajiPokok = bahanPerhitunganGaji[0][indeksKaryawan];
-                        double upahLembur = bahanPerhitunganGaji[1][indeksKaryawan] * jamLembur;
-                        double pajak = bahanPerhitunganGaji[2][indeksKaryawan];
-                        double gajiBersih = (gajiPokok + upahLembur) - ((gajiPokok + upahLembur) * pajak);
-
-                        System.out.printf("%-20s: %.0f%s\n", "Pajak", (pajak*100), "%");
-
-                        tampilGaji(gajiBersih, name);
-
-                        }
-                    }
-                    case 0 -> berhenti = true;
-                    default -> System.out.println("Tidak terdeteksi, silakan input lagi..");
-                }
-            }
-        }
-        public static void tampilNama(String[][] dataString, long[][] dataLong, char[] dataChar) {
-            String urutanST = "", urutanSRC = "", urutanAdm = "";
-            for (int i = 0; i < dataString[2].length; i++) {
-                if (dataString[2][i].equalsIgnoreCase("Supir Travel")) urutanST += i + " ";
-                else if (dataString[2][i].equalsIgnoreCase("Supir Rent Car")) urutanSRC += i + " ";
-                else if (dataString[2][i].equalsIgnoreCase("Admin")) urutanAdm += i + " ";
-            }
+                    if (dataChar.length > 0) {
+                        System.out.println("DAFTAR GAJI POKOK:");
+                        System.out.println("- Supir Travel   : Rp. 1.500.000");
+                        System.out.println("- Supir Rent Car : Rp. 1.750.000");
+                        System.out.println("- Admin          : Rp. 2.000.000");
+                        System.out.println("=========================================");
+                        System.out.printf("%-20s: ", "Nama Karyawan"); String name = in.nextLine();
+                        System.out.printf("%-20s: ", "Kategori"); String kategori = in.nextLine();
+                        int indeksKaryawan = cariKaryawan(name, dataString, kategori, daftarKategori);
+                        
+                        if (indeksKaryawan == -1) {
     
-            String[] indeksST = urutanST.split(" ");
-            String[] indeksSRC = urutanSRC.split(" ");
-            String[] indeksAdm = urutanAdm.split(" ");
+                            System.out.println("Karyawan tidak ditemukan.");
     
-            System.out.println("\nSUPIR TRAVEL");
-            if (!urutanST.isEmpty()) {
-                for (int i = 0; i < dataChar.length; i++) {
-                    for (int j = 0; j < indeksST.length; j++) {
-                        if (i == Integer.parseInt(indeksST[j])) {
-                            System.out.printf("%-20s: %s\n", "Nama", dataString[0][i]);
-                            System.out.printf("%-20s: %s\n", "Alamat", dataString[1][i]);
-                            System.out.printf("%-20s: %s\n", "Kategori Karyawan", dataString[2][i]);
-                            System.out.printf("%-20s: %s\n", "Shift", dataString[3][i]);
-                            System.out.printf("%-20s: %s\n", "Nama Bank", dataString[4][i]);
-                            System.out.printf("%-20s: %d\n", "No. Telepon", dataLong[0][i]);
-                            System.out.printf("%-20s: %d\n", "No.Rekening", dataLong[1][i]);
-                            System.out.printf("%-20s: %c\n\n", "Jenis Kelamin", dataChar[i]);
-                        }
-                    }
-                }
-            } else System.out.println("-\n");
-            
+                        } else {
+                            inputLagiBenar = false;
+                            while (!inputLagiBenar) {
+                                System.out.printf("%-20s: ", "Gaji Pokok"); int gajiPokok = in.nextInt();
+                                for (int i = 0; i < bahanPerhitunganGaji[0].length; i++) {
+                                    if (gajiPokok == bahanPerhitunganGaji[0][indeksKaryawan]) {
+                                        inputLagiBenar = true;
+                                    }
+                                }
+                                if (inputLagiBenar == false) System.out.println("Gaji Pokok tidak sesuai dengan kategori.\n");
+                            } inputLagiBenar = false; 
+                            
+                            System.out.printf("%-20s: ", "Jam Lembur"); int jamLembur = in.nextInt();
     
-            System.out.println("\nSUPIR RENT CAR");
-            if (!urutanSRC.isEmpty()) {
-                for (int i = 0; i < dataChar.length; i++) {
-                    for (int j = 0; j < indeksSRC.length; j++) {
-                        if (i == Integer.parseInt(indeksSRC[j])) {
-                            System.out.printf("%-20s: %s\n", "Nama", dataString[0][i]);
-                            System.out.printf("%-20s: %s\n", "Alamat", dataString[1][i]);
-                            System.out.printf("%-20s: %s\n", "Kategori Karyawan", dataString[2][i]);
-                            System.out.printf("%-20s: %s\n", "Shift", dataString[3][i]);
-                            System.out.printf("%-20s: %s\n", "Nama Bank", dataString[4][i]);
-                            System.out.printf("%-20s: %d\n", "No. Telepon", dataLong[0][i]);
-                            System.out.printf("%-20s: %d\n", "No.Rekening", dataLong[1][i]);
-                            System.out.printf("%-20s: %c\n\n", "Jenis Kelamin", dataChar[i]);
-                        }
-                    }
-                }
-            } else System.out.println("-\n");
-            
+                            double gajiPokok = bahanPerhitunganGaji[0][indeksKaryawan];
+                            double upahLembur = bahanPerhitunganGaji[1][indeksKaryawan] * jamLembur;
+                            double pajak = bahanPerhitunganGaji[2][indeksKaryawan];
+                            double gajiBersih = (gajiPokok + upahLembur) - ((gajiPokok + upahLembur) * pajak);
     
-            System.out.println("\nADMIN");
-            if (!urutanAdm.isEmpty()) {
-                for (int i = 0; i < dataChar.length; i++) {
-                    for (int j = 0; j < indeksAdm.length; j++) {
-                        if (i == Integer.parseInt(indeksAdm[j])) {
-                            System.out.printf("%-20s: %s\n", "Nama", dataString[0][i]);
-                            System.out.printf("%-20s: %s\n", "Alamat", dataString[1][i]);
-                            System.out.printf("%-20s: %s\n", "Kategori Karyawan", dataString[2][i]);
-                            System.out.printf("%-20s: %s\n", "Shift", dataString[3][i]);
-                            System.out.printf("%-20s: %s\n", "Nama Bank", dataString[4][i]);
-                            System.out.printf("%-20s: %d\n", "No. Telepon", dataLong[0][i]);
-                            System.out.printf("%-20s: %d\n", "No.Rekening", dataLong[1][i]);
-                            System.out.printf("%-20s: %c\n\n", "Jenis Kelamin", dataChar[i]);
-                        }
-                    }
-                }
-            } else System.out.println("-\n");
-        }
+                            System.out.printf("%-20s: %.0f%s\n", "Pajak", (pajak*100), "%");
     
-        public static int cariKaryawan(String name, String[][] dataString, String kategori, String[] daftarKategori) {
-            for (int i = 0; i < dataString[0].length; i++) {
-                if (dataString[0][i].equalsIgnoreCase(name)) {
-                    for (int j = 0; j < dataString[2].length; j++) {
-                        if (kategori.equalsIgnoreCase(dataString[2][i])) {
-                            for (int j2 = 0; j2 < daftarKategori.length; j2++) {
-                                if (kategori.equalsIgnoreCase(daftarKategori[j2])) return j2;
+                            tampilGaji(gajiBersih, name);
+    
                             }
-                        }
+
+                    } else System.out.println("Karyawan Kosong!\n");
+
                     }
-                }
+                case 0 -> berhenti = true;
+                default -> System.out.println("Tidak terdeteksi, silakan input lagi..");
             }
-            return -1;
+        }
+
+    }
+
+
+    public static void tampilNama(String[][] dataString, long[][] dataLong, char[] dataChar) {
+
+        String urutanST = "", urutanSRC = "", urutanAdm = "";
+        for (int i = 0; i < dataString[2].length; i++) {
+            if (dataString[2][i].equalsIgnoreCase("Supir Travel")) urutanST += i + " ";
+            else if (dataString[2][i].equalsIgnoreCase("Supir Rent Car")) urutanSRC += i + " ";
+            else if (dataString[2][i].equalsIgnoreCase("Admin")) urutanAdm += i + " ";
         }
     
-        public static void tampilGaji(double gajiBersih, String name) {
-            System.out.printf("Gaji Bersih %s adalah Rp.%.0f\n\n", name, gajiBersih);
+        String[] indeksST = urutanST.split(" ");
+        String[] indeksSRC = urutanSRC.split(" ");
+        String[] indeksAdm = urutanAdm.split(" ");
+            
+        System.out.println("+=========================================+");
+        System.out.printf("|%-41s|\n"," SUPIR TRAVEL");
+        if (!urutanST.isEmpty()) {
+            for (int i = 0; i < dataChar.length; i++) {
+                for (int j = 0; j < indeksST.length; j++) {
+                    if (i == Integer.parseInt(indeksST[j])) {
+                        System.out.println("|-----------------------------------------|");
+                        System.out.printf("|%-19s: %-20s|\n", "Nama", dataString[0][i]);
+                        System.out.printf("|%-19s: %-20s|\n", "Alamat", dataString[1][i]);
+                        System.out.printf("|%-19s: %-20s|\n", "Kategori Karyawan", dataString[2][i]);
+                        System.out.printf("|%-19s: %-20s|\n", "Shift", dataString[3][i]);
+                        System.out.printf("|%-19s: %-20s|\n", "Nama Bank", dataString[4][i]);
+                        System.out.printf("|%-19s: %-20d|\n", "No. Telepon", dataLong[0][i]);
+                        System.out.printf("|%-19s: %-20d|\n", "No.Rekening", dataLong[1][i]);
+                        System.out.printf("|%-19s: %-20c|\n", "Jenis Kelamin", dataChar[i]);
+                    }
+                }
+            } System.out.println("+=========================================+");
+        } else {
+            System.out.println("|-----------------------------------------|");
+            System.out.printf("|%-41s|\n"," -");
+            System.out.println("+=========================================+");
+        }
+            
+
+        System.out.println("\n+=========================================+");
+        System.out.printf("|%-41s|\n"," SUPIR RENT CAR");
+        if (!urutanSRC.isEmpty()) {
+            for (int i = 0; i < dataChar.length; i++) {
+                for (int j = 0; j < indeksSRC.length; j++) {
+                    if (i == Integer.parseInt(indeksSRC[j])) {
+                        System.out.println("|-----------------------------------------|");
+                        System.out.printf("|%-19s: %-20s|\n", "Nama", dataString[0][i]);
+                        System.out.printf("|%-19s: %-20s|\n", "Alamat", dataString[1][i]);
+                        System.out.printf("|%-19s: %-20s|\n", "Kategori Karyawan", dataString[2][i]);
+                        System.out.printf("|%-19s: %-20s|\n", "Shift", dataString[3][i]);
+                        System.out.printf("|%-19s: %-20s|\n", "Nama Bank", dataString[4][i]);
+                        System.out.printf("|%-19s: %-20d|\n", "No. Telepon", dataLong[0][i]);
+                        System.out.printf("|%-19s: %-20d|\n", "No.Rekening", dataLong[1][i]);
+                        System.out.printf("|%-19s: %-20c|\n", "Jenis Kelamin", dataChar[i]);
+                    }
+                }
+            } System.out.println("+=========================================+");
+        } else {
+            System.out.println("|-----------------------------------------|");
+            System.out.printf("|%-41s|\n"," -");
+            System.out.println("+=========================================+");
+        }
+            
+    
+        System.out.println("\n+=========================================+");
+        System.out.printf("|%-41s|\n"," ADMIN");
+        if (!urutanAdm.isEmpty()) {
+            for (int i = 0; i < dataChar.length; i++) {
+                for (int j = 0; j < indeksAdm.length; j++) {
+                    if (i == Integer.parseInt(indeksAdm[j])) {
+                        System.out.println("|-----------------------------------------|");
+                        System.out.printf("|%-19s: %-20s|\n", "Nama", dataString[0][i]);
+                        System.out.printf("|%-19s: %-20s|\n", "Alamat", dataString[1][i]);
+                        System.out.printf("|%-19s: %-20s|\n", "Kategori Karyawan", dataString[2][i]);
+                        System.out.printf("|%-19s: %-20s|\n", "Shift", dataString[3][i]);
+                        System.out.printf("|%-19s: %-20s|\n", "Nama Bank", dataString[4][i]);
+                        System.out.printf("|%-19s: %-20d|\n", "No. Telepon", dataLong[0][i]);
+                        System.out.printf("|%-19s: %-20d|\n", "No.Rekening", dataLong[1][i]);
+                        System.out.printf("|%-19s: %-20c|\n", "Jenis Kelamin", dataChar[i]);
+                    }
+                }
+            } System.out.println("+=========================================+");
+        } else {
+            System.out.println("|-----------------------------------------|");
+            System.out.printf("|%-41s|\n"," -");
+            System.out.println("+=========================================+");
         }
     }
+    
+    public static int cariKaryawan(String name, String[][] dataString, String kategori, String[] daftarKategori) {
+        for (int i = 0; i < dataString[0].length; i++) {
+            if (dataString[0][i].equalsIgnoreCase(name)) {
+                for (int j = 0; j < dataString[2].length; j++) {
+                    if (kategori.equalsIgnoreCase(dataString[2][i])) {
+                        for (int j2 = 0; j2 < daftarKategori.length; j2++) {
+                            if (kategori.equalsIgnoreCase(daftarKategori[j2])) return j2;
+                        }
+                    }
+                }
+            }
+        }
+        return -1;
+    }
+    
+    public static void tampilGaji(double gajiBersih, String name) {
+        System.out.printf("Gaji Bersih %s adalah Rp.%.0f\n\n", name, gajiBersih);
+    }
+}
